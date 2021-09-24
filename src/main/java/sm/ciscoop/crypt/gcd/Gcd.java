82068fc113bdc714b2ac9b9ff76a18543529f334
Claudio Gennari <clagenna@gmail.com>
2021-09-24 19:13:52 +0200

    aggiunta un po di javadoc

--- src/main/java/sm/ciscoop/crypt/gcd/Gcd.java
+++ src/main/java/sm/ciscoop/crypt/gcd/Gcd.java
@@ -7,11 +7,22 @@ public class Gcd {
   /**
    * per l'originale vedi la <A href=
    * "https://introcs.cs.princeton.edu/java/99crypto/ExtendedEuclid.java.html">seguente
-   * pagina</a>
+   * pagina</a><br/>
+   * 
+   * L'algoritmo euclideo esteso &egrave; particolarmente utile quando
+   * <code>a</code> e <code>b</code> sono <b>coprimi</b> (o mcd &egrave; = 1)
+   * poiché <code>x</code> &egrave; l'inverso moltiplicativo modulare di
+   * "<code>a mod b</code>", e <code>y</code> &egrave; l'inverso moltiplicativo
+   * modulare di "<code>b mod a</code>". <br/>
+   * Il calcolo dell'inverso moltiplicativo modulare &egrave; un passaggio
+   * essenziale nel metodo di crittografia a chiave pubblica <b>RSA</b>.
    *
    * @param a
+   *          primo intero
    * @param b
-   * @return
+   *          secondo intero
+   * @return un record {@link GcdRec} che contiene il resto e i due valori di x
+   *         e y
    */
   public GcdRec gcd(BigInteger a, BigInteger b) {
     if (b == null || b.equals(BigInteger.ZERO))
--- src/main/java/sm/ciscoop/crypt/gcd/GcdRec.java
+++ src/main/java/sm/ciscoop/crypt/gcd/GcdRec.java
@@ -1,7 +1,11 @@
 package sm.ciscoop.crypt.gcd;
 
 import java.math.BigInteger;
-
+/**
+ * 
+ * @author claudio
+ *
+ */
 public record GcdRec(BigInteger resto, BigInteger x, BigInteger y) {
 
 }
